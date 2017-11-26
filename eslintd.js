87691795985b5c435b4e44beb099b75351c100b6
Michael Mitchell <michaelmitchell@users.noreply.github.com>
2017-11-26 22:14:49 +0200

    load and unload plugins properly

--- eslintd.js
+++ eslintd.js
@@ -2,13 +2,26 @@
  *
  */
 define(function(require, exports, module) {
-    main.consumes = ["language"];
-    main.provides = [];
+    main.consumes = ["language", "Plugin"];
+    main.provides = ["eslintd"];
 
     function main(options, imports, register) {
-        imports.language.registerLanguageHandler("plugins/c9.ide.language.javascript.eslintd/worker/eslintd_worker");
+        var Plugin = imports.Plugin;
+        var language = imports.language;
+        
+        //
+        var plugin = new Plugin("eslintd", main.consumes);
+        
+        plugin.on("load", function () {
+            language.unregisterLanguageHandler("plugins/c9.ide.language.javascript.eslint/worker/eslint_worker");
+            language.registerLanguageHandler("plugins/c9.ide.language.javascript.eslintd/worker/eslintd_worker");
+        });
+        plugin.on("unload", function () {
+            language.unregisterLanguageHandler("plugins/c9.ide.language.javascript.eslintd/worker/eslintd_worker");
+            language.registerLanguageHandler("plugins/c9.ide.language.javascript.eslint/worker/eslint_worker");
+        });
 
-        register(null, {});
+        register(null, { eslintd: plugin });
     }
 
     return main;
